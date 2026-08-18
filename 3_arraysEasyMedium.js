80b855f20c2016335fcaef7a514eba0e15b9ddfc
Pravin Mishra <43815390+pravin-mishra123@users.noreply.github.com>
2026-08-19 00:53:14 +0530

    missing number comment code

--- 3_arraysEasyMedium.js
+++ 3_arraysEasyMedium.js
@@ -277,20 +277,20 @@
 //-------------------------------------------------------------------------------
 
 // 8.Missing Number
-function missingNumber(nums){
-    let n = nums.length;
-    let totalSum = n * (n + 1) / 2;
-    let partialSum = 0;
+// function missingNumber(nums){
+//     let n = nums.length;
+//     let totalSum = n * (n + 1) / 2;
+//     let partialSum = 0;
 
-    for(let i = 0; i < nums.length; i++){
-        partialSum = partialSum + nums[i]
-    }
+//     for(let i = 0; i < nums.length; i++){
+//         partialSum = partialSum + nums[i]
+//     }
 
-    return totalSum - partialSum;
+//     return totalSum - partialSum;
     
-}
+// }
 
-console.log(missingNumber([4,0,1,2,5]))
+// console.log(missingNumber([4,0,1,2,5]))
 
 // Practice Similar questions
 // - https://leetcode.com/problems/first-missing-positive/description/
